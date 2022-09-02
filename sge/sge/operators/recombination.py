77c4d9eae729c9a9aaa7aa71eddcf29fcf09ce2e
Jessica <jessicac@student.dei.uc.pt>
2022-09-02 18:35:55 +0100

    minor update

--- sge/sge/operators/recombination.py
+++ sge/sge/operators/recombination.py
@@ -15,11 +15,9 @@ def crossover(p1, p2):
     mapping_values = [0] * gen_size
     # check parent with highest fitness
     # off spring inherits the grammar of the best fitted parent
-    if p1['fitness'] < p2['fitness']:
-        gram = p1['grammar']
-    else:
-        gram = p2['grammar']
+    # TODO: change this in case of maximization problem
+    gram = p1['pcfg'] if p1['fitness'] < p2['fitness'] else p2['pcfg']
    
     # compute nem individual
     _, tree_depth = grammar.mapping(genotype, gram, mapping_values)
-    return {'genotype': genotype, 'fitness': None, 'mapping_values': mapping_values, 'tree_depth': tree_depth, 'grammar': gram}
+    return {'genotype': genotype, 'fitness': None, 'mapping_values': mapping_values, 'tree_depth': tree_depth, 'pcfg': gram}
