5125b25e30063cfb4c42e17edcc227c31d1bd864
Nuno Lourenço <namlourenco@gmail.com>
2023-04-05 11:00:17 +0100

    Simple reg linear.

--- sge/examples/simple_symreg.py
+++ sge/examples/simple_symreg.py
@@ -8,7 +8,7 @@ class SimpleSymbolicRegression():
         self.fitness_cases = num_fitness_cases
         self.x_points = np.asarray([x for x in range(self.fitness_cases)])
         self.y_points = np.asarray([self.function(x) for x in self.x_points])
-        self.x_evals = np.empty(self.fitness_cases)
+        self.outputs = np.empty(self.fitness_cases)
 
     def evaluate(self, individual):
         try:
@@ -17,8 +17,16 @@ class SimpleSymbolicRegression():
             locals_code = {}
             exec(code, globals_code, locals_code)
             func = locals_code['result']
-            self.x_evals = np.apply_along_axis(func, 0, self.x_points)
-            error = np.sum(np.sqrt(np.square(self.x_evals - self.y_points)))
+            self.outputs = np.apply_along_axis(func, 0, self.x_points)
+            print(self.outputs)
+            covariance_y_o = np.cov(np.vstack((self.y_points, self.outputs)))
+            variance_o = np.var(self.outputs)
+            b = covariance_y_o / variance_o
+            a = self.y_points.mean() - b * self.outputs.mean()
+            print(b)
+            print(a)
+            input()
+            error = np.sum(np.sqrt(np.square(self.outputs - self.y_points)))
         except (OverflowError, ValueError) as e:
             error = self.invalid_fitness
         if np.isnan(error):
