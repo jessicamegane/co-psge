827ce7d5d2cd0cbdfed058859b02f3efd7f69cff
jessicamegane <jessicac@dei.uc.pt>
2024-03-06 00:17:33 +0000

    improved grammar mutation code

--- sge/sge/operators/update.py
+++ sge/sge/operators/update.py
@@ -2,8 +2,7 @@ import numpy as np
 
 def grammar_mutation(ind, prob_mutation, gauss_std):
     ind['fitness'] = None
-    gram = ind['pcfg']
-    for nt_values in gram:
+    for idx_nt, nt_values in enumerate(ind['pcfg']):
         if len(nt_values)  <= 1:
             continue
         for prob_idx in range(len(nt_values)):
@@ -12,6 +11,8 @@ def grammar_mutation(ind, prob_mutation, gauss_std):
                 diff = (gauss / (len(nt_values) - 1))
                 nt_values[prob_idx] += (gauss + diff)
                 nt_values -= diff
-                nt_values = np.clip(nt_values, 0, np.infty) / np.sum(np.clip(nt_values, 0, np.infty))
+                nt_values = np.clip(nt_values, 0, np.infty)
+                nt_values /= np.sum(np.clip(nt_values, 0, np.infty))
+                ind['pcfg'][idx_nt] = nt_values
                 break
     return ind
